7ba49729bff39b1fb1381c70b62228a306e4b25e
TomGroombridge <groombridgetom@gmail.com>
2014-03-28 10:22:36 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WoodPellets::Application.config.secret_key_base = 'cc7480f81ef2b3cb5ad694f152a127a5bb915c6ebb1eb0216c49367bf55eba78d4b451d021b8683bf9279a5df55a60e2f70292f59fe9ae38943137b982c8370e'
